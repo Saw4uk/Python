d18e1da0984df99008743241c2e97e8ff28105a0
Saw4uk <87923228+Saw4uk@users.noreply.github.com>
2022-12-17 16:16:06 +0500

    VacanciesSplit

--- pythonProject/6.3.py
+++ pythonProject/6.3.py
@@ -1,5 +1,6 @@
 import csv
 import re
+import os
 import matplotlib.pyplot as plt
 import numpy as np
 from openpyxl.styles.borders import Border, Side
@@ -17,6 +18,36 @@ format_money = re.compile('\d+(\.\d{1,2})?')
 
 
 # Строка для конфликта значение ветка develop
+class Splitter:
+    __final_files_dict = {}
+
+    @staticmethod
+    def splitCSV(file_name):
+        """Разделяет большой csv файл на маленькие файлы - группы по годам
+
+        :param file_name:
+            название файла
+        :type file_name: str
+        """
+        dirname = "SplittedFile"
+        if not os.path.exists(dirname):
+            os.mkdir(dirname)
+        with open(file_name, encoding="utf-8-sig") as csvfile:
+            file_name = f"{dirname}/{file_name.split('/')[-1].split('.')[0]}"
+            if not os.path.exists(file_name):
+                os.mkdir(file_name)
+            reader = csv.DictReader(csvfile)
+            name_of_fields = list(reader.fieldnames)
+            for row in reader:
+                year = row["published_at"].split("-")[0]
+                if year not in Splitter.__final_files_dict:
+                    new_name_file = f"{file_name}/{year}.csv"
+                    new_file = open(new_name_file, "w+", encoding="utf-8-sig", newline='')
+                    Splitter.__final_files_dict[year] = csv.DictWriter(new_file, name_of_fields)
+                    Splitter.__final_files_dict[year].writeheader()
+
+                Splitter.__final_files_dict[year].writerow(row)
+
 
 class DataSet:
 
@@ -375,6 +406,7 @@ class StartProgramm:
     def __init__(self):
         file_name = "vacancies_by_year.csv"  # ("Введите название файла: ")
         vacancy_name = "Аналитик"  # input("Введите название профессии: ")
+        Splitter.splitCSV(file_name)
         data_set = DataSet(file_name)
         stat = Statistics(vacancy_name, data_set)
         stat.print_formatted()
